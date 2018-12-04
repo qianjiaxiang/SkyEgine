d0f53955b622860c47229ed132cd097eadab747b
SC-201712291012\Administrator <895239330@qq.com>
2018-12-04 16:39:05 +0800

    springboot init
    springboot初始化

---
+++ src/main/java/java/com/skyengine/SkyengineApplication.java
@@ -0,0 +1,12 @@
+package java.com.skyengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyengineApplication.class, args);
+	}
+}
